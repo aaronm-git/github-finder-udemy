7a806c3e3ca21e4545d4b29a76f8f1b27d173124
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2022-06-11 16:32:04 -0400

    added spinner

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import UserItem from "./UserItem";
 import PropTypes from "prop-types";
+import Spinner from "../layout/Spinner";
 
-const Users = ({ users }) => {
-    return (
-        <div style={userStyle}>
-            {users.map(user => (
-                <UserItem key={user.id} user={user} />
-            ))}
-        </div>
-    );
+const Users = ({ loading, users }) => {
+    if (loading) {
+        return <Spinner />;
+    } else {
+        return (
+            <div style={userStyle}>
+                {users.map(user => (
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+        );
+    }
 };
 
 Users.propTypes = {
