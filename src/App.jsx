44b27a42fd1564dd97ec756d39376ede565d9d93
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2022-06-11 16:23:14 -0400

    github api vars added

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ class App extends React.Component {
 
     async componentDidMount() {
         this.setState({ loading: true });
-        // const res = await axios.get("https://api.github.com/users");
+        this.setState({ users: userObject });
+        // const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
         // console.log(res.data);
         // this.setState({ users: res.data });
-        this.setState({ users: userObject });
         this.setState({ loading: false });
     }
     render() {
--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
     return (
         <div className='card text-center'>
-            <img src={avatar_url} alt='' className='round-img' style={{ width: "60px" }} />
+            <img src={avatar_url} alt={login + " profile picture"} className='round-img' style={{ width: "60px" }} />
             <h3>{login}</h3>
 
             <div>
